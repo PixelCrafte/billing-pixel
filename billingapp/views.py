ca2f92e381221b2543e97c3f3047f0cacca622b5
worshix <worshiplivingstone@gmail.com>
2025-08-22 05:05:02 +0200

    about to move to auth

--- billingapp/views.py
+++ billingapp/views.py
@@ -1,1 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404, redirect
+from .models import *
+from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, View
+
+class HomeView(View):
+    def get(self, request):
+        return render(request, "billingapp/home.html")
