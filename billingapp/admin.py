5d42069186b841f6fe2a27e59fedcf3074035f9a
worshix <worshiplivingstone@gmail.com>
2025-08-22 04:46:41 +0200

    created superuser and admin panel

--- billingapp/admin.py
+++ billingapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Company, Client, Invoice, Quotation
 
-# Register your models here.
+admin.site.register(Company)
+admin.site.register(Client)
+admin.site.register(Invoice)
+admin.site.register(Quotation)
