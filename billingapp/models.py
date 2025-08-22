6a7235f8fc1e7e7c7a73a138cd93d46df33250e7
worshix <worshiplivingstone@gmail.com>
2025-08-22 04:42:19 +0200

    modelling complete

--- billingapp/models.py
+++ billingapp/models.py
@@ -6,6 +6,24 @@ from django.db.models import Max
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+    
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        related_name='billingapp_user_set',
+        related_query_name='billingapp_user',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name='billingapp_user_set',
+        related_query_name='billingapp_user',
+    )
+    
     def __str__(self):
         return self.username
     
